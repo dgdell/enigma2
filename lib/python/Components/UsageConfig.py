a4570666fa7eb61880a3da4a800f6551e66ebb02
dgdell <dgdell@126.com>
2016-11-27 21:28:07 +0800

    Update UsageConfig.py
    ("chi zho", _("Simplified Chinese")) fix China Language

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -576,7 +576,7 @@ def InitUsageConfig():
 		("eus baq", _("Basque")),
 		("bul", _("Bulgarian")),
 		("hrv", _("Croatian")),
-		("chn sgp", _("Simplified Chinese")),
+		("chi zho", _("Simplified Chinese")),
 		("twn hkn",_("Traditional Chinese")),
 		("ces cze", _("Czech")),
 		("dan", _("Danish")),
